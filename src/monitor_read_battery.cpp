b74063c29164952652da92536323c0a5bc0086f7
Patrick K. Moffitt <Patrick@Moffitt.com>
2018-05-24 11:22:51 -0400

    Correct math error.

--- src/monitor_read_battery.cpp
+++ src/monitor_read_battery.cpp
@@ -26,14 +26,14 @@ int get_battery_vdc() {
     // Read the battery level from the ESP8266 analog in pin.
     // Analog read level is 10 bit 0-1023 (0V-1V).
     // our 10MΩ & 2.2MΩ voltage divider takes the max
-    // lipo value of 4.2V and drops it to 0.848V max.
+    // lipo value of 4.2V and drops it to 0.757V max.
     // this means our min analog read value should be 566 (3.14V)
-    // and the max analog read value should be 755 (4.2V).
+    // and the max analog read value should be 757 (4.2V).
     const size_t readings_len{30};
     std::array<int, readings_len> readings;
     std::fill_n(begin(readings), readings_len, 0);
     for (int i=0; i < (int) readings_len; i++) {
-        readings[i] = analogRead(A0);
+        readings[i] = lround(analogRead(A0) * 0.97656);
         delay(33);
     }
     int sum = accumulate(begin(readings), end(readings), 0, std::plus<int>());
@@ -41,7 +41,7 @@ int get_battery_vdc() {
     Serial.print("Raw ADC value: ");
     Serial.println(level);
     // convert battery level to percent
-    level = map(level, 566, 755, 0, 100);
+    level = map(level, 566, 757, 0, 100);
     Serial.print("Battery level: ");
     Serial.print(level);
     Serial.println("%");
